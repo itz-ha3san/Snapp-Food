b6a26c3b07a0ed48a904477517d271c1f8598065
hassan <gjfjkk173@gmail.com>
2026-06-29 21:47:29 +0330

    first

---
+++ src/main/java/com/example/snappfood/SnappFoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.snappfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnappFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnappFoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/snappfood/SnappFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snappfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnappFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
